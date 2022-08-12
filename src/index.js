543f284c58dae2fdc029bbc4cb566f511d99080a
Subhojit Saha <subhojitan@gmail.com>
2022-08-12 14:44:24 +0200

    First commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,117 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const listContainerStyle = {
+  width: '30%', 
+  margin: '50px auto 0 auto', 
+  fontSize: '1.5em', 
+  fontWeight: 'bold'
+}
+
+
+const MyFirstReactApp = () => {
+  const [storageLength, setStorageLength] = useState(0);
+
+  useEffect(() => {
+    setStorageLength(prev => prev + localStorage.length)
+  }, []);
+
+  const [userInput, setUserInput] = useState('');
+  const [tasks, setTasks] = useState([])
+
+  useEffect(() => {
+    const allTasks = localStorage.getItem('Tasks');
+    setTasks(JSON.parse(allTasks))
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem('Tasks', JSON.stringify(tasks));
+  }, [tasks])
+
+  const handleAddClick = () => {
+    if(userInput.length < 1) {
+      alert('Text field cannot be empty')
+      return
+    }
+    setTasks((prev) => 
+      prev ? [...tasks, {
+        id: storageLength + 1,
+        text: userInput 
+      }] : [{
+        id: storageLength + 1,
+        text: userInput 
+      }]
+    );
+    setStorageLength(storageLength+1)
+    setUserInput('');
+  }
+
+  const handleRemoveClick = (idToRemove) => {
+    console.log(idToRemove)
+    const newTask = tasks.filter(item => item.id !== idToRemove);
+    console.log(newTask);
+    setTasks(newTask)
+  }
+
+  const handleKeyPress = (e) => {
+    if(e.key === 'Enter') {
+      handleAddClick();
+    }
+  }
+
+  return (
+    <div className='container' style={{backgroundColor: '#9DC5BB', color: 'white'}}>
+      <h1>Create your To-Do list</h1>
+      <div className='userInput'>
+        <input placeholder='Empty your mind' value={userInput} onChange={(e) => setUserInput(e.target.value)} onKeyPress={handleKeyPress} style={{border: 'none', borderRadius: '10px'}}/>
+        <button 
+        onClick={handleAddClick} 
+        style={{backgroundColor: '#5e807f', color: 'white', fontWeight: 'Bold'}} 
+        className='Add'
+        onMouseEnter={({target}) => target.style.backgroundColor = '#527170'}
+        onMouseLeave={({target}) => target.style.backgroundColor = '#5e807f'}
+        >
+          ADD
+        </button>
+      </div>
+      <div className='listContainer' style={listContainerStyle}>
+        <ol>
+          {tasks && tasks.map(item => <List Task={item} removeTask={handleRemoveClick} />)}
+        </ol>
+      </div>
+    </div>
+  )
+}
+
+const List = (props) => {
+  const {Task, removeTask} = props;
+
+  const handleClick = () => {
+    removeTask(Task.id);
+  }
+
+  return(
+    <li style={{marginTop: '20px', backgroundColor: '#5e807f', color: 'whitesmoke', border: 'none', borderRadius: '10px'}}>
+      {Task.text}
+      <button 
+        className='delete' 
+        style={{ float:'right', margin:'5px', background: 'none', border:'none', borderRadius: '50%'}} 
+        onClick={handleClick}
+        onMouseEnter={({target}) => target.style.background = 'white'}
+        onMouseLeave={({target}) => target.style.background = 'none'}
+      >
+        &times;
+      </button>
+    </li>
+  )
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <MyFirstReactApp />,
   document.getElementById('root')
-);
+)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
+
